24911cdaf18aedb24ab366a0b0e7de2d109460d0
lemolatoon <63438515+lemolatoon@users.noreply.github.com>
2021-08-26 11:33:25 +0900

    learn tuple

--- hello-world/main.rs
+++ hello-world/main.rs
@@ -1,18 +1,6 @@
 fn main() {
-    // Specify the data type "char"
-    let char_1: char = 'S';
-    let char_2: char = 'f';
-    // one quote
+    // Tuple of length 3
+    let tuple_e = ("e", 5i32, true);
 
-    // Compiler interprets a single item in quotations as the "char" data type
-    let smiley_face = "☺";
-
-    // Compiler interprets a series of items in quotations as a "str" data type and creates a "&str" reference
-    let string_1 = "miley ";
-
-
-    // Specify the data type "str" with the reference synta "&str"
-    let string_2: &str = "ace";
-
-    println!("{} is a {}{}{}{}.", smiley_face, char_1, string_1, char_2, string_2)
+    println!("Is '{}' the {}th letter of the alphabet? {}", tuple_e.0, tuple_e.1, tuple_e.2);
 }

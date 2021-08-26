83393e2de0731e7777794545fc7033ba85aed7ef
lemolatoon <63438515+lemolatoon@users.noreply.github.com>
2021-08-26 11:30:39 +0900

    learn about char &str

--- hello-world/main.rs
+++ hello-world/main.rs
@@ -1,6 +1,18 @@
 fn main() {
-    // compile error
-    let number: i8 = 128;
+    // Specify the data type "char"
+    let char_1: char = 'S';
+    let char_2: char = 'f';
+    // one quote
 
-    println!("The number is {}", number);
+    // Compiler interprets a single item in quotations as the "char" data type
+    let smiley_face = "☺";
+
+    // Compiler interprets a series of items in quotations as a "str" data type and creates a "&str" reference
+    let string_1 = "miley ";
+
+
+    // Specify the data type "str" with the reference synta "&str"
+    let string_2: &str = "ace";
+
+    println!("{} is a {}{}{}{}.", smiley_face, char_1, string_1, char_2, string_2)
 }

74364a3c51ce0f7e88d3d18282da49ed2df9f569
lemolatoon <63438515+lemolatoon@users.noreply.github.com>
2021-08-26 12:03:57 +0900

    cargo, debug, if statement

--- hello-rust/src/main.rs
+++ hello-rust/src/main.rs
@@ -1,3 +1,17 @@
 fn main() {
-    println!("Hello, world!");
+    if 1 == 2 {
+        println!("True, the numbers are equal.");
+    } else {
+        println!("False, the numbers are not equal");
+    }
+
+    let formal = true;
+
+    let greeting = if formal { // if used hre as an expression
+        "Good day to you."     // return a String
+    } else {
+        "Hey!"                 // return a String
+    };
+
+    println!("{}", greeting)   // prints "Good day to you."
 }

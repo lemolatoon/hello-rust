556715bae507bfe35fc02a4e8c9a4555129861ef
lemolatoon <63438515+lemolatoon@users.noreply.github.com>
2021-08-26 10:22:39 +0900

    Delete hello-rust/target directory

--- hello-rust/target/debug/deps/hello_rust-07c5cd6ff31f1520.d
+++
@@ -1,5 +0,0 @@
-/home/kaisei/rust/hello-rust/hello-rust/target/debug/deps/hello_rust-07c5cd6ff31f1520: src/main.rs
-
-/home/kaisei/rust/hello-rust/hello-rust/target/debug/deps/hello_rust-07c5cd6ff31f1520.d: src/main.rs
-
-src/main.rs:
--- hello-rust/target/debug/hello-rust.d
+++
@@ -1,1 +0,0 @@
-/home/kaisei/rust/hello-rust/hello-rust/target/debug/hello-rust: /home/kaisei/rust/hello-rust/hello-rust/src/main.rs

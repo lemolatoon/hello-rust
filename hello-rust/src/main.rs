e97da8198eac88c2f2e78c3bd1d965415af49914
lemolatoon <63438515+lemolatoon@users.noreply.github.com>
2021-08-26 13:44:55 +0900

    learn arrays

--- hello-rust/src/main.rs
+++ hello-rust/src/main.rs
@@ -1,34 +1,14 @@
 
 fn main() {
-    let mut car = car_factory(String::from("Red"), Transmission::Manual, false);
-    println!("Car 1 = {}, {:?} transmission, conertible: {}, mileage: {}", car.color, car.transmission, car.convertible, car.mileage);
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 
-    car = car_factory(String::from("Silver"), Transmission::Automatic, true);
-    println!("Car 2 = {}, {:?} transmission, conertible: {}, mileage: {}", car.color, car.transmission, car.convertible, car.mileage);
+    let bytes = [0; 5];
 
-    car = car_factory(String::from("Yellow"), Transmission::SemiAuto, false);
-    println!("Car 2 = {}, {:?} transmission, conertible: {}, mileage: {}", car.color, car.transmission, car.convertible, car.mileage);
-}
 
-fn car_factory(color: String, transmission: Transmission, convertible: bool) -> Car {
-    let car = Car {color: color, transmission: transmission, convertible: convertible, mileage: 0};
+    let first = days[0];
 
-    car
-}
+    let second = days[1];
 
-// Declare Car struct to describe vehicle with four named fields
-struct Car {
-    color: String,
-    transmission: Transmission,
-    convertible: bool,
-    mileage: u32,
-}
-
-#[derive(PartialEq, Debug)]
-// Declare enum for Car transmission type
-// TO DO: Fix enum definition so code compiles
-enum Transmission {
-    Manual,
-    SemiAuto,
-    Automatic
+    let seventh = days[6];
+    println!("{}", seventh)
 }

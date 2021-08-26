113534e8c46a09e6fbe9504c42e468e53eb174f5
lemolatoon <63438515+lemolatoon@users.noreply.github.com>
2021-08-26 12:42:50 +0900

    learn struct

--- hello-rust/src/main.rs
+++ hello-rust/src/main.rs
@@ -1,8 +1,21 @@
 fn main() {
+    // Instantiate classic struct, specify fields in random order, or in specified orfer
+    let user_1 = Student {name: String::from("Constance Sharma"), remote: true, level: 2};
+    let user_2 = Student {name: String::from("Dyson Tan"), level: 5, remote: false};
+
+    // Instantiate tuple structs, pass values in same orfer as types defined
+    let mark_1 = Grades('A', 'A', 'B', 'A', 3.75);
+    let mark_2 = Grades('B', 'A', 'B', 'C', 3.25);
+
+    println!("{}, level {}. Remote: {}. Grades: {}, {}, {}, {}. Average: {}",
+             user_1.name, user_1.level, user_1.remote, mark_1.0, mark_1.1, mark_1.2, mark_1.3, mark_1.4);
+    println!("{}, level {}. Remote: {}. Grades: {}, {}, {}, {}. Average: {}",
+             user_2.name, user_2.level, user_2.remote, mark_2.0, mark_2.1, mark_2.2, mark_2.3, mark_2.4);
+
 }
 
 // Classic struct with named fields
-struct Student {name: String, level: u8, pass: bool}
+struct Student {name: String, level: u8, remote: bool}
 
 // Tuple struct with data types only
 struct Grades(char, char, char, char, f32);

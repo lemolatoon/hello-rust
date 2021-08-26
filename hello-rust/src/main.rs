ccdcb9649ecfa5f66967dcfc8a073b7bfd3f2ea9
lemolatoon <63438515+lemolatoon@users.noreply.github.com>
2021-08-26 13:39:41 +0900

    practice struct by using car

--- hello-rust/src/main.rs
+++ hello-rust/src/main.rs
@@ -1,38 +1,34 @@
 
-fn goodbye() {
-    println!("Goodbye!");
-}
-
 fn main() {
-    println!("Hellow, world");
-    goodbye();
-    is_divisible_by(12, 4);
-    is_divisible_by(13, 5);
-    is_divisible_by(14, 0);
+    let mut car = car_factory(String::from("Red"), Transmission::Manual, false);
+    println!("Car 1 = {}, {:?} transmission, conertible: {}, mileage: {}", car.color, car.transmission, car.convertible, car.mileage);
 
-    if is_zero(0) {
-        println!("The value is zero.");
-    }
+    car = car_factory(String::from("Silver"), Transmission::Automatic, true);
+    println!("Car 2 = {}, {:?} transmission, conertible: {}, mileage: {}", car.color, car.transmission, car.convertible, car.mileage);
+
+    car = car_factory(String::from("Yellow"), Transmission::SemiAuto, false);
+    println!("Car 2 = {}, {:?} transmission, conertible: {}, mileage: {}", car.color, car.transmission, car.convertible, car.mileage);
 }
 
-fn is_zero(input: u8) -> bool {
-    if input == 0 {
-        return true;
-    }
-    false
+fn car_factory(color: String, transmission: Transmission, convertible: bool) -> Car {
+    let car = Car {color: color, transmission: transmission, convertible: convertible, mileage: 0};
+
+    car
 }
 
-fn is_divisible_by(dividend: u32, divisor: u32) -> bool {
-    // If the divisor is zero, stop execution
-    if divisor == 0 {
-        println!("\nError! Divison by zero is not allowed.");
-        return false;
-    } else if dividend % divisor > 0 {
-        println!("\n{} % {} has a remainder of {}.", dividend, divisor, (dividend % divisor));
-    } else {
-        println!("\n{} % {} has no remainder.", dividend, divisor);
-    }
+// Declare Car struct to describe vehicle with four named fields
+struct Car {
+    color: String,
+    transmission: Transmission,
+    convertible: bool,
+    mileage: u32,
+}
 
-    // Create the boolea value and return it to the function caller
-    dividend % divisor == 0
+#[derive(PartialEq, Debug)]
+// Declare enum for Car transmission type
+// TO DO: Fix enum definition so code compiles
+enum Transmission {
+    Manual,
+    SemiAuto,
+    Automatic
 }

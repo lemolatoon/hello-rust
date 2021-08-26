1a934a7185653f90b2a1814a6fc5b567f5fdd066
lemolatoon <63438515+lemolatoon@users.noreply.github.com>
2021-08-26 13:22:32 +0900

    learn function

--- hello-rust/src/main.rs
+++ hello-rust/src/main.rs
@@ -1,45 +1,38 @@
+
+fn goodbye() {
+    println!("Goodbye!");
+}
+
 fn main() {
+    println!("Hellow, world");
+    goodbye();
+    is_divisible_by(12, 4);
+    is_divisible_by(13, 5);
+    is_divisible_by(14, 0);
 
-    let click = MouseClick {x: 100, y: 250};
-    println!("Mouse click location: {}, {}", click.x, click.y);
-    let keys = KeyPress(String::from("Ctrl+"), 'N');
-    println!("\n Keys pressed: {}{}", keys.0, keys.1);
-
-    let we_load = WebEvent2::WELoad(true);
-    let we_click = WebEvent2::WEClick(click);
-    let we_key = WebEvent2::WEKeys(keys);
-
-    println!("\nWebEvent enum structure: \n\n {:#?} \n\n{:#?} \n\n{:#?}", we_load, we_click, we_key)
-    
+    if is_zero(0) {
+        println!("The value is zero.");
+    }
 }
 
-// Classic struct with named fields
-struct Student {name: String, level: u8, remote: bool}
-
-// Tuple struct with data types only
-struct Grades(char, char, char, char, f32);
-
-// Unit struct
-struct Unit;
-
-enum WebEvent {
-    // An enum variant can be like a unit struct without fields or data types
-    WELoad,
-    // An enum variant can be like a tuple struct with data types but no named fields
-    WEKeys(String, char),
-    // An enu variant can be like a classic struct with named fields and their data types
-    WEClick {x: i64, y: i64}
+fn is_zero(input: u8) -> bool {
+    if input == 0 {
+        return true;
+    }
+    false
 }
 
-// Define a tuple struct
-#[derive(Debug)]
-struct KeyPress(String, char);
+fn is_divisible_by(dividend: u32, divisor: u32) -> bool {
+    // If the divisor is zero, stop execution
+    if divisor == 0 {
+        println!("\nError! Divison by zero is not allowed.");
+        return false;
+    } else if dividend % divisor > 0 {
+        println!("\n{} % {} has a remainder of {}.", dividend, divisor, (dividend % divisor));
+    } else {
+        println!("\n{} % {} has no remainder.", dividend, divisor);
+    }
 
-// Define a classic struct
-#[derive(Debug)]
-struct MouseClick {x: i64, y: i64}
-
-// Redefine the enum variants to use the data from th new structs
-// Update the page Load variant to have the boolean type
-#[derive(Debug)]
-enum WebEvent2 {WELoad(bool), WEClick(MouseClick), WEKeys(KeyPress)}
+    // Create the boolea value and return it to the function caller
+    dividend % divisor == 0
+}
